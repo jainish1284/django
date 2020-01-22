58c8965ffe909912e9d7c91aaf00cc3c1509b4b9
alterworlddevelopers <alterworlddevelopers@gmail.com>
2020-01-23 00:51:56 +0530

    home

--- django_ecom/main/models.py
+++ django_ecom/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Tutorials(models.Model):
+    tutorial_title = models.CharField(max_length=200)
+    tutorial_content = models.TextField()
+    tutorial_published = models.DateTimeField("Published Date", default=datetime.now())
+
+    def __str__(self):
+        return self.tutorial_content
+
